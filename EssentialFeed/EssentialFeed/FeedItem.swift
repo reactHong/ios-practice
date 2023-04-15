2b85448a482e9269039da3e215472994ba05bdb0
Hong Shik Kim <hong@hongs.dev>
2023-04-15 07:47:47 -0700

    feat: Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Hong Shik Kim on 2023/04/15.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+
