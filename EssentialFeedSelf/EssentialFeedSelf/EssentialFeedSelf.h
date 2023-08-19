e0b27863128c232c031f9f19246200ea4821b1c6
Hong Shik Kim <hong@hongs.dev>
2023-08-19 15:52:16 -0700

    add: Create EssentialFeedSelf macOS framework

---
+++ EssentialFeedSelf/EssentialFeedSelf/EssentialFeedSelf.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedSelf.h
+//  EssentialFeedSelf
+//
+//  Created by Hong Shik Kim on 2023/08/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedSelf.
+FOUNDATION_EXPORT double EssentialFeedSelfVersionNumber;
+
+//! Project version string for EssentialFeedSelf.
+FOUNDATION_EXPORT const unsigned char EssentialFeedSelfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedSelf/PublicHeader.h>
+
+
