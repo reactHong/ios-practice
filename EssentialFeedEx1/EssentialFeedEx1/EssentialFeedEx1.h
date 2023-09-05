4d7e16aca71dd0b89dce971f351ee362e49e26b4
Hong Shik Kim <hong@hongs.dev>
2023-09-05 07:22:05 -0700

    add: Add a new macOS framework project, EssentialFeedEx1

---
+++ EssentialFeedEx1/EssentialFeedEx1/EssentialFeedEx1.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedEx1.h
+//  EssentialFeedEx1
+//
+//  Created by Hong Shik Kim on 2023/09/05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedEx1.
+FOUNDATION_EXPORT double EssentialFeedEx1VersionNumber;
+
+//! Project version string for EssentialFeedEx1.
+FOUNDATION_EXPORT const unsigned char EssentialFeedEx1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedEx1/PublicHeader.h>
+
+
