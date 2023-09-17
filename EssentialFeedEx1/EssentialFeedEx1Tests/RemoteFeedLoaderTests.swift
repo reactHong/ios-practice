2a74cdb6bb4ff691f3c8b4e45099d7ce3e49a0f0
Hong Shik Kim <hong@hongs.dev>
2023-09-16 22:38:00 -0700

    test: Request data from URL on 'load'

--- EssentialFeedEx1/EssentialFeedEx1Tests/RemoteFeedLoaderTests.swift
+++ EssentialFeedEx1/EssentialFeedEx1Tests/RemoteFeedLoaderTests.swift
@@ -18,21 +18,37 @@ import EssentialFeedEx1
 */
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoader {
-    
+    func load(completion: ()->Void) {
+        HTTPClient.shared.requestedURL = URL(string: "https://any-url.com")!
+        
+    }
 }
 
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load { }
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
