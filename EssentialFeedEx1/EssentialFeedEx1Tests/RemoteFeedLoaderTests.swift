7062fc0de793d78127959c2f3338c7dc56cb9622
Hong Shik Kim <hong@hongs.dev>
2023-09-18 22:10:17 -0700

    refactor: Make HTTPClient protocol

--- EssentialFeedEx1/EssentialFeedEx1Tests/RemoteFeedLoaderTests.swift
+++ EssentialFeedEx1/EssentialFeedEx1Tests/RemoteFeedLoaderTests.swift
@@ -16,17 +16,27 @@ import EssentialFeedEx1
  - HTTPClient에는 URL 정보가 있어야 한다.
 
 */
+/*
+ Goal: Use dependecy injection instead of Global shared variable
+ Why: RemoteFeedLoader does not need to know about HTTPClient.shared
+ */
 
-class HTTPClient {
-    static var shared = HTTPClient()
-    
-    func get(from: URL) {}
+
+protocol HTTPClient {
+    func get(from: URL)
 }
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load() {
         //HTTPClient.shared.requestedURL = URL(string: "https://any-url.com")!
-        HTTPClient.shared.get(from: URL(string: "https://any-url.com")!)
+        //HTTPClient.shared.get(from: URL(string: "https://any-url.com")!)
+        client.get(from: URL(string: "https://any-url.com")!)
         
     }
 }
@@ -34,7 +44,7 @@ class RemoteFeedLoader {
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
-    override func get(from url: URL) {
+    func get(from url: URL) {
         requestedURL = url
     }
 }
@@ -44,16 +54,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestsDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         
         sut.load()
         
